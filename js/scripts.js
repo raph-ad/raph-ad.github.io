c8d2ec1d458b048fa3d386ec0ff967366a9ed4ea
raphtsang <raphael@ad-group.com.au>
2023-09-14 15:54:40 +1000

    sticky form respect resize

--- js/scripts.js
+++ js/scripts.js
@@ -21,14 +21,62 @@ document.addEventListener('DOMContentLoaded', (event) => {
 //   }
 // }
 
-window.onscroll = function() {hideViewGallery()};
+
+window.onscroll = function() {
+    hideViewGallery();
+    checkScroll()
+};
+
 
 var viewGallery = document.getElementById("view-gallery");
 
 function hideViewGallery () {
-    if (window.scrollY == 0) {
+    if (window.scrollY < 5) {
         viewGallery.classList.remove("hide")
     } else {
         viewGallery.classList.add("hide")
     }; 
 };
+
+var form = document.getElementById("form");
+var coord = form.getBoundingClientRect();
+
+window.addEventListener("resize", (event) => {
+    coord = form.getBoundingClientRect();
+    console.log("resize");
+});
+
+
+function checkScroll () {
+    if (window.scrollY + 52 >= coord.top) {
+        // form.style.backgroundColor = "#000000";
+        form.style.position = "fixed";
+        form.style.top = "0";
+        form.style.bottom = "auto";
+        console.log("if");
+        console.log(window.scrollY);
+    } 
+        else {
+        // form.style.backgroundColor = "#ff0000"
+        form.style.position = "absolute";
+        form.style.bottom = "6.8rem";
+        form.style.top = "auto";
+        console.log("else");
+          console.log(window.scrollY);
+      }
+};
+
+// console.log(window.scrollY);
+    // console.log(form.offsetTop);
+    // if ((window.scrollY + 52) >= (form.offsetTop)) {
+    //     form.style.position = "fixed";
+    //         form.style.top = "0";
+    //         form.style.bottom = "auto";
+    //         console.log("else");
+    // }
+    // else if ((window.scrollY + 52) <= (form.offsetTop)) {
+    //     form.style.position = "absolute";
+    //     form.style.bottom = "6.8rem";
+    //     form.style.top = "auto";
+    //     console.log("if");
+    // }
